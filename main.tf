cc68829d2b9f0bbf2182a086f6a105436003f9fd
GeeBee <47665346+biradarganesh5@users.noreply.github.com>
2024-05-17 23:45:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,67 @@
 
+provider "aws" {
+
+  region = "us-west-2" # Set your desired AWS region
+  access_key = "AWS_ACCESS_KEY_ID"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
+}
+
+resource "aws_ecs_cluster" "hello_world_cluster" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "hello_world_task" {
+  family                   = "hello-world-task"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = <<DEFINITION
+  [
+    {
+      "name": "hello-world-container",
+      "image": "node:14",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ]
+    }
+  ]
+  DEFINITION
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "hello-world-ecs-task-execution-role"
+  assume_role_policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  POLICY
+}
+
+resource "aws_ecs_service" "hello_world_service" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.hello_world_cluster.id
+  task_definition = aws_ecs_task_definition.hello_world_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = ["subnet-0af857850cc317c68"] # Add your subnet ID
+    security_groups = ["sg-0bae9cee9f52b5899"]     # Add your security group ID
+    assign_public_ip = true
+  }
+}
